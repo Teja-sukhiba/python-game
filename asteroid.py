38c143a05cf8ab42b577ca27be26734a0005adbe
Tejas <tejaspvg@gmail.com>
2025-09-20 21:57:14 +0530

    add ability to shot and destory the asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 
 from circleshape import CircleShape
+from constants import *
+import random
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
@@ -11,3 +13,16 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
     self.position += (self.velocity * dt)
+
+  def split(self):
+    self.kill()
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return
+    random_angle = random.uniform(20, 50)
+    velocity_1 = self.velocity.rotate(random_angle)
+    velocity_2 = self.velocity.rotate(-random_angle)
+    new_radius = self.radius - ASTEROID_MIN_RADIUS
+    new_asteroid_1 =  Asteroid(self.position.x, self.position.x, new_radius)
+    new_asteroid_2 =  Asteroid(self.position.x, self.position.x, new_radius)
+    new_asteroid_1.velocity = velocity_1 * 1.2
+    new_asteroid_2.velocity = velocity_2 * 1.2
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
 import pygame
 
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     # initialize the pygame module
@@ -21,12 +23,14 @@ def main():
     drawable = pygame.sprite.Group()
 
     asteroids = pygame.sprite.Group()
-    # asteroid_field = pygame.sprite.Group()
+
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-    
+
     Player.containers = (updatable, drawable)
     player = Player(x, y)
     
@@ -37,6 +41,16 @@ def main():
         
          # --- Update all sprites in updatable ---
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         # --- Draw all sprites in drawable individually ---
         screen.fill("black")
